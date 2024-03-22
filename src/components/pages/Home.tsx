c2407b4b3150d824284ddc22a5c9d7e89199dbf8
kev1nnnnnnn <kevinbfv@gmail.com>
2024-03-22 17:58:34 -0300

    site finalizado

---
+++ src/components/pages/Home.tsx
@@ -0,0 +1,19 @@
+import About from "../organs/About"
+import Blogs from "../organs/Blogs"
+import Contact from "../organs/Contact"
+import HeroSection from "../organs/HeroSection"
+
+
+
+const Home = () => {
+    return (
+        <>
+            <HeroSection />
+            <About />
+            <Blogs />
+            <Contact />
+        </>
+    )
+}
+
+export default Home
---
+++ src/components/particles/Data.tsx
@@ -0,0 +1,195 @@
+export const NavLinks = [
+    {
+        name: "Home",
+        url: "/"
+    }
+   
+];
+
+
+
+export const HeroTexts = [
+    {
+        Heading: "Monitoramento da qualidade do ar",
+        Paragraph: "Junte-se ao esforço mundial para garantir um ar mais limpo e saudável para todos! Faça parte dessa iniciativa global e contribua para a proteção da saúde pública.",
+        Button: "Saiba mais!"
+    },
+    {
+        Heading: "Registros diários!",
+        Paragraph: "Registre diariamente a qualidade do ar para um ambiente mais saudável! Acompanhe os níveis de poluentes e contribua com dados valiosos para pesquisas e autoridades. Promova a conscientização sobre a importância do ar limpo para a saúde de todos.",
+        Button: "Saiba mais!"
+    },
+    {
+        Heading: "Navegação!",
+        Paragraph: "Descubra a qualidade do ar em diferentes áreas navegando pelo mapa. Tome medidas informadas para promover um ambiente mais saudável para você e para o planeta. Junte-se a nós nesta jornada de conscientização e ação ambiental!",
+        Button: "Saiba mais!"
+    }
+]
+
+
+export const AboutTexts = {
+    firstText: "sobre nós",
+    secondText: "Bem-vindo!",
+    caption: "Bem-vindo ao nosso site de monitoramento da qualidade do ar!",
+    paragraph1: "site de monitoramento da qualidade do ar oferece dados em tempo real e análises abrangentes sobre a poluição atmosférica em todo o mundo. Com mapas interativos e informações detalhadas, ajudamos você a entender os níveis de poluentes e a tomar medidas para proteger sua saúde e o meio ambiente. Junte-se a nós nesta missão de promover ar mais limpo e comunidades mais saudáveis.",
+}
+
+export const OfferTexts = {
+    firstText: "What we do",
+    secondText: "All Our Offer",
+    list: [
+        {
+            listCaption: "Weight Loose Programs",
+            text: "Our personalized approach, expert guidance, and proven results will help you achieve your fitness goals. Take the first step towards a transformed body and lifestyle today.",
+        },
+        {
+            listCaption: "Body Building Programs",
+            text: "Our expert trainers will guide you through tailored workouts, helping you achieve your bodybuilding goals efficiently and safely.",
+        }
+        , {
+            listCaption: "Different Special Classes",
+            text: "Take your workout to new heights with our high and intense Special Class, where you'll push your limits and achieve remarkable fitness results.",
+        }
+    ],
+}
+
+export const MembershipPlans = {
+    firstText: "Pricing Tables",
+    secondText: "Membership Plans",
+    cards: [
+        {
+            amount: 8,
+            duration: "day",
+            caption: "One Day Training",
+            benefits: ["One time access to all clubs", "Group trainer", "Book a Group class", "Fitness orientation"]
+        },
+        {
+            amount: 49,
+            duration: "month",
+            caption: "12 Months Membership",
+            benefits: ["Group classes", "Discuss fitness goals", "Group trainer", "Fitness orientation"]
+        },
+        {
+            amount: 65,
+            duration: "month",
+            caption: "Pay Every Month",
+            benefits: ["Group classes", "Discuss fitness goals", "Group trainer", "Fitness orientation"]
+        }
+    ]
+}
+
+export const ContactTexts = {
+    firstText: "um pouco mais sobre",
+    phone: "Monitoramento Global",
+    paragraph: "O monitoramento global da qualidade do ar é crucial para proteger a saúde e o meio ambiente, fornecendo dados em tempo real sobre os níveis de poluentes em diferentes regiões.",
+    button: "Saiba mais"
+}
+
+export const TestimonialTexts = {
+    firstText: "our testimonials",
+    secondText: "What Clients Say",
+    feedBacks: [
+        {
+            text: "I've been a member for six months, and the gym's supportive community and knowledgeable trainers have helped me surpass my fitness goals!",
+            person: "Darrell Murray",
+            type: "Client"
+        },
+        {
+            text: "The Yoga classes have transformed my life; I feel more centered, strong, and peaceful every time I step on the mat.",
+            person: "Derrick Rodriquez",
+            type: "Client"
+        },
+        {
+            text: "Attending the Special Class has been a game-changer! It's intense, but the results are incredible. Highly recommended!",
+            person: "Collins Thompson",
+            type: "Client"
+        }
+    ],
+}
+
+export const BlogTexts = {
+    firstText: "informe-se",
+    secondText: "Nossas últimas notícias",
+    blogNews: [
+        {
+            caption: "Preservar",
+            title: "Preservar o meio ambiente",
+            paragraph: "Preservar o meio ambiente é essencial para garantir um futuro sustentável para as próximas gerações. Ao proteger ecossistemas naturais, reduzir o consumo de recursos e adotar práticas sustentáveis, podemos contribuir para a conservação da biodiversidade e a mitigação das mudanças climáticas. Juntos, podemos criar um mundo mais verde e equilibrado.",
+            time: "1m",
+            author: "John Kevin",
+            comments: "21"
+        },
+        {
+            caption: "Plantar + para  o futuro",
+            title: "Plantar para garantirmos um futuro melhor",
+            paragraph: "Plantar árvores hoje é investir em um futuro mais verde e saudável para todos. Cada árvore plantada não apenas contribui para a melhoria da qualidade do ar e do solo, mas também oferece habitats para a vida selvagem e ajuda a reduzir os efeitos das mudanças climáticas. Ao plantarmos juntos, estamos cultivando um legado sustentável para as próximas gerações.",
+            time: "15m",
+            author: "Armando Neto",
+            comments: "45"
+        },
+        {
+            caption: "Como somos afetados",
+            title: "Riscos que corremos devido a qualidade do ar",
+            paragraph: "A qualidade do ar ruim pode ter sérios impactos em nossa saúde e bem-estar. A exposição a poluentes atmosféricos pode aumentar o risco de doenças respiratórias, como asma e bronquite, além de causar irritação nos olhos e na garganta. Além disso, a poluição do ar está associada a problemas cardiovasculares e pode até mesmo prejudicar o desenvolvimento cognitivo em crianças. Garantir ar limpo é essencial para proteger nossa saúde e qualidade de vida.",
+            time: "19m",
+            author: "Marcus Said",
+            comments: "55"
+        }
+    ]
+}
+
+export const FooterTexts = {
+    underLogoText: "O trabalho transversal relacionado ao meio ambiente e à poluição do ar visa abordar questões interdisciplinares para promover a qualidade do ar e a sustentabilidade ambiental. Isso envolve a colaboração entre diversos setores, como governos, indústrias, comunidades locais e organizações não governamentais",
+    quickLinks: {
+        caption: "Equipe",
+        links: [
+            {
+                name: "John Kevin",
+                url: "/"
+            },
+            {
+                name: "Alecxander Vitor",
+                url: "/"
+            },
+            {
+                name: "Armando Neto",
+                url: "/"
+            },
+            {
+                name: "Guilherme Castro",
+                url: "/"
+            },
+            {
+                name: "Kethleen Cristina",
+                url: "/"
+            },
+            {
+                name: "Marcos V. Said",
+                url: "/"
+            },
+            {
+                name: "Rafel Pereira",
+                url: "/"
+            },
+            {
+                name: "Rilker Roberto",
+                url: "/"
+            },
+        ]
+    },
+    contacts: {
+        caption: "Informações",
+        names: [
+            {
+                name: "FAMETRO",
+            },
+            {
+                name: "7 período",
+            },
+            {
+                name: "Noturno",
+            }
+        ]
+    },
+    copyright: "Copyright 2023. All Right Reserved"
+}
